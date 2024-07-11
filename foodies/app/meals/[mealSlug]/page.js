7ddc15b1e8b220647ad63afb25b18b18ab0ac6f2
alexander herrera <chander.zap@gmail.com>
2024-07-11 14:18:55 -0600

    [add] - giving some love to meal details page

---
+++ foodies/app/meals/[mealSlug]/page.js
@@ -0,0 +1,42 @@
+import styles from './page.module.css'
+
+import Image from 'next/image.js'
+import { getMeal } from '@/lib/meals.js'
+import { notFound } from 'next/navigation.js'
+
+export default function MealDetails ({ params }) {
+  const data = getMeal(params.mealSlug)
+
+  if (!data) {
+    notFound()
+  }
+
+  data.instructions = data.instructions.replace(/\n/g, '<br>')
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image src={data.image} alt={`Image of ${data.title}`} fill />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{data.title}</h1>
+          <p className={styles.creator}>
+            By <a href={`mailto:${data.creator_email}`}> {data.creator} </a>
+          </p>
+          <p className={styles.sumary}>
+            {data.summary}
+          </p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{
+            __html: data.instructions
+          }}
+        />
+      </main>
+    </>
+  )
+}
---
+++ foodies/app/meals/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound () {
+  return (
+    <main className='not-found'>
+      <h1>MEAL NOT FOUND</h1>
+      <p>We were not able to found the requested meal</p>
+    </main>
+  )
+}
