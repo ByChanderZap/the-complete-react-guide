2f1e8a78aef061d4547ddc898915e056935dfa30
alexander herrera <chander.zap@gmail.com>
2024-07-11 13:56:33 -0600

    [add] - a simple error handling, i swear this is magic

---
+++ foodies/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound () {
+  return (
+    <main className='not-found'>
+      <h1>PAGE NOT FOUND</h1>
+      <p>Cannout found requested resource</p>
+    </main>
+  )
+}
