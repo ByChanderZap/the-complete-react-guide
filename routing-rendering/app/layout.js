87dbe0d5f07f9d040b84d96ca2c19d33a4010e62
alexander herrera <chander.zap@gmail.com>
2024-07-13 01:31:19 -0600

    [add] - sorry i forget to fix all linter things

--- routing-rendering/app/layout.js
+++ routing-rendering/app/layout.js
@@ -1,13 +1,13 @@
-import './globals.css';
+import './globals.css'
 
 export const metadata = {
   title: 'Next.js Page Routing & Rendering',
-  description: 'Learn how to route to different pages.',
+  description: 'Learn how to route to different pages.'
 }
 
-export default function RootLayout({ children }) {
- return (
-    <html lang="en">
+export default function RootLayout ({ children }) {
+  return (
+    <html lang='en'>
       <body>{children}</body>
     </html>
   )
--- routing-rendering/app/news/page.js
+++ routing-rendering/app/news/page.js
@@ -1,7 +1,7 @@
-export default function NewsPage() {
+export default function NewsPage () {
   return (
-  <>
+    <>
 
-  </>
+    </>
   )
 }
