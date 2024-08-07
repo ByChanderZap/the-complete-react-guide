13e81542a4ddc1715d7470efa525296edcfa783d
alexander herrera <chander.zap@gmail.com>
2024-08-07 13:53:57 -0600

    [add] - metadata

--- optimizations/app/feed/page.js
+++ optimizations/app/feed/page.js
@@ -1,6 +1,17 @@
 import Posts from '@/components/posts'
 import { getPosts } from '@/lib/posts'
 
+// This is to have dynamic metadata
+export async function generateMetadata () {
+  const posts = await getPosts()
+  const postsLenght = posts.length
+
+  return {
+    title: `Browse all our ${postsLenght} posts`,
+    description: 'Browse all our posts'
+  }
+}
+
 export default async function FeedPage () {
   const posts = await getPosts()
   return (
--- optimizations/app/page.js
+++ optimizations/app/page.js
@@ -1,23 +1,28 @@
-import { Suspense } from 'react';
+import { Suspense } from 'react'
 
-import Posts from '@/components/posts';
-import { getPosts } from '@/lib/posts';
+import Posts from '@/components/posts'
+import { getPosts } from '@/lib/posts'
 
-async function LatestPosts() {
-  const latestPosts = await getPosts(2);
-  return <Posts posts={latestPosts} />;
+export const metadata = {
+  title: 'Latest posts',
+  description: 'Browse our latest posts'
 }
 
-export default async function Home() {
+async function LatestPosts () {
+  const latestPosts = await getPosts(2)
+  return <Posts posts={latestPosts} />
+}
+
+export default async function Home () {
   return (
     <>
       <h1>Welcome back!</h1>
       <p>Here's what you might've missed.</p>
-      <section id="latest-posts">
-      <Suspense fallback={<p>Loading recent posts...</p>}>
-        <LatestPosts />
-      </Suspense>
+      <section id='latest-posts'>
+        <Suspense fallback={<p>Loading recent posts...</p>}>
+          <LatestPosts />
+        </Suspense>
       </section>
     </>
-  );
+  )
 }
